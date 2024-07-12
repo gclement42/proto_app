597024c5a8f248686b16de3bee10d4f919ba0199
Clement Gerardin <clementgerardin.cg@gmail.com>
2024-07-12 14:07:40 +0200

    add profile image

--- app/src/main/java/com/example/lunabee_proto/MainActivity.kt
+++ app/src/main/java/com/example/lunabee_proto/MainActivity.kt
@@ -1,15 +1,36 @@
 package com.example.lunabee_proto
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val loginButton: Button = findViewById(R.id.login_button)
+        loginButton.setOnClickListener {
+            authenticateUser()
+        }
+    }
+
+    private fun authenticateUser() {
+        val usernameEditText: TextView = findViewById(R.id.edit_text_username)
+        val passwordEditText: TextView = findViewById(R.id.edit_text_password)
+        val username = usernameEditText.text.toString()
+        val password = passwordEditText.text.toString()
+        if (username.isEmpty() or password.isEmpty()) {
+            Log.d("MainActivity", "Username or password is empty")
+        }
+        else {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
     }
 }
