5b62b6ed58e3959d1d07ff877a0e8822e9d73a9d
Clement Gerardin <clementgerardin.cg@gmail.com>
2024-07-19 13:38:09 +0200

    Custom cover in list

--- app/src/main/java/ListAdapter.kt
+++ app/src/main/java/ListAdapter.kt
@@ -2,6 +2,7 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lunabee_proto.AlbumActivity
@@ -13,6 +14,7 @@ class ListAdapter(private val albums: List<AlbumData>):
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val name: TextView = itemView.findViewById(R.id.itemTitle)
         val artist: TextView = itemView.findViewById(R.id.itemArtist)
+        val cover: ImageView = itemView.findViewById(R.id.itemCover)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -23,6 +25,7 @@ class ListAdapter(private val albums: List<AlbumData>):
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.name.text = albums[position].getName()
         holder.artist.text = albums[position].getArtist()
+        holder.cover.setImageResource(albums[position].getImage())
         holder.itemView.setOnClickListener {
             val intent = Intent(holder.itemView.context, AlbumActivity::class.java).apply {
                 putExtra("ALBUM_TITLE", albums[position].getName())
